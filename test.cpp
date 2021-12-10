345195606225ce80bec5be6a0c70fae22eb15582
Miodrag Milanovic <mmicko@gmail.com>
2021-12-10 17:22:52 +0100

    read data

--- test.cpp
+++ test.cpp
@@ -23,6 +23,7 @@ class FstData
 
     void extractVarNames();
 
+    void callback(uint64_t pnt_time, fstHandle pnt_facidx, const unsigned char *pnt_value, uint32_t plen);
   private:
     struct fstReaderContext *ctx;
     std::vector<std::string> scopes;
@@ -48,6 +49,26 @@ static std::string remove_spaces(std::string str)
 {
     str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
     return str;
+}
+
+void FstData::callback(uint64_t pnt_time, fstHandle pnt_facidx, const unsigned char *pnt_value, uint32_t plen)
+{
+    FstVar var = handle_to_var[pnt_facidx];
+    printf("%zu %s = %s\n", pnt_time, var.name.c_str(), pnt_value);
+}
+
+
+static void value_change_callback_varlen(void *user_data, uint64_t pnt_time, fstHandle pnt_facidx, const unsigned char *pnt_value, uint32_t plen)
+{
+    FstData *ptr = (FstData*)user_data;
+    ptr->callback(pnt_time, pnt_facidx, pnt_value, plen);
+}
+
+static void value_change_callback(void *user_data, uint64_t pnt_time, fstHandle pnt_facidx, const unsigned char *pnt_value)
+{
+    FstData *ptr = (FstData*)user_data;
+    uint32_t plen = (pnt_value) ?  strlen((const char *)pnt_value) : 0;
+    ptr->callback(pnt_time, pnt_facidx, pnt_value, plen);
 }
 
 void FstData::extractVarNames()
@@ -84,6 +105,9 @@ void FstData::extractVarNames()
     for (auto &val : handle_to_var) {
         printf("var : %s.%s %d\n", val.second.scope.c_str(), val.second.name.c_str(), (int)val.second.aliases.size());
     }
+
+    fstReaderSetFacProcessMaskAll(ctx);
+    fstReaderIterBlocks2(ctx, value_change_callback, value_change_callback_varlen, this, nullptr);
 }
 
 /*
